f40663a21886bde7d0f518d000ece3a6026be4eb
Dave Scotese <dscotese@litmocracy.com>
2020-09-14 16:58:11 -0700

    Update plugin.js
    Added rest-cn to hold CN to be used on self-signed certificate.

--- plugin.js
+++ plugin.js
@@ -7,6 +7,7 @@ restPlugin.addOption('rest-port', 3001, 'rest plugin listens on this port', 'int
 restPlugin.addOption('rest-docport', 4001, 'rest plugin listens on this port', 'int');
 restPlugin.addOption('rest-protocol', 'https', 'rest plugin protocol', 'string');
 restPlugin.addOption('rest-execmode', 'production', 'rest exec mode', 'string');
+restPlugin.addOption('rest-cn', 'localhost', 'CN for certificate', 'string');
 restPlugin.addOption('rest-rpc', ' ', 'allowed rpc commands', 'string');
 
 restPlugin.onInit = params => {
